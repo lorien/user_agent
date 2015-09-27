c5388c8fa05d3ae1311185e09aea514ccc363f78
Gregory Petukhov <lorien@lorien.name>
2015-09-28 01:50:43 +0600

    Add navigator option. Add chrome builds generator.

--- test.py
+++ test.py
@@ -2,14 +2,15 @@
 from unittest import TestCase
 import unittest
 
-from user_agent import generate_user_agent, generate_navigator
+from user_agent import (generate_user_agent, generate_navigator,
+                        UserAgentRuntimeError)
 
 class UserAgentTestCase(TestCase):
     def test_it(self):
         ua = generate_user_agent()
         self.assertTrue(len(ua) > 0)
 
-    def test_navigator_option(self):
+    def test_platform_option(self):
         for x in range(100):
             ua = generate_user_agent(platform='linux')
             self.assertTrue('linux' in ua.lower())
@@ -19,6 +20,18 @@ class UserAgentTestCase(TestCase):
 
             ua = generate_user_agent(platform='mac')
             self.assertTrue('mac' in ua.lower())
+
+            self.assertRaises(UserAgentRuntimeError,
+                              generate_user_agent,
+                              platform=11)
+
+    def test_navigator_option(self):
+        for x in range(100):
+            ua = generate_user_agent(navigator='firefox')
+            self.assertTrue('firefox' in ua.lower())
+
+            ua = generate_user_agent(navigator='chrome')
+            self.assertTrue('chrome' in ua.lower())
 
 
 if __name__ == '__main__':
--- user_agent/base.py
+++ user_agent/base.py
@@ -7,10 +7,11 @@ TODO:
 * You can build completely random user-agent or limit generation
 to specific OS, browser, etc.
 """
-from random import choice
+from random import choice, randint
 import six
 
-__all__ = ['generate_user_agent', 'generate_navigator']
+__all__ = ['generate_user_agent', 'generate_navigator',
+           'UserAgentRuntimeError']
 
 PLATFORM = ('linux', 'mac', 'win')
 NAVIGATOR = ('firefox', 'chrome')
@@ -57,30 +58,81 @@ FIREFOX_VERSION = (
     '39.0','40.0','41.0','42.0','43.0',
 )
 GECKOTRAIL_DESKTOP = '20100101'
-CHROME_VERSION = (
-#    '27.0.1453.116','27.0.1453.90','27.0.1453.93','27.0.1500.55',
-#    '28.0.1464.0','28.0.1467.0','28.0.1468.0','29.0.1547.2','29.0.1547.57',
-#    '29.0.1547.62','30.0.1599.17','31.0.1623.0','31.0.1650.16','32.0.1664.3',
-#    '32.0.1667.0','33.0.1750.517','34.0.1847.116','34.0.1847.137','34.0.1866.237',
-    '35.0.1916.47','35.0.2117.157','35.0.2309.372','35.0.3319.102','36.0.1944.0',
-    '36.0.1985.125','36.0.1985.67','37.0.2049.0','37.0.2062.124','41.0.2224.3',
-    '41.0.2225.0','41.0.2226.0','41.0.2227.1','41.0.2228.0','41.0.2272.76',
-    '42.0.2311.1', '43.0.2357.134', '44.0.2403.130', '45.0.2454.85',
+#CHROME_VERSION = (
+##    '27.0.1453.116','27.0.1453.90','27.0.1453.93','27.0.1500.55',
+##    '28.0.1464.0','28.0.1467.0','28.0.1468.0','29.0.1547.2','29.0.1547.57',
+##    '29.0.1547.62','30.0.1599.17','31.0.1623.0','31.0.1650.16','32.0.1664.3',
+##    '32.0.1667.0','33.0.1750.517','34.0.1847.116','34.0.1847.137','34.0.1866.237',
+#    '35.0.1916.47','35.0.2117.157','35.0.2309.372','35.0.3319.102','36.0.1944.0',
+#    '36.0.1985.125','36.0.1985.67','37.0.2049.0','37.0.2062.124','41.0.2224.3',
+#    '41.0.2225.0','41.0.2226.0','41.0.2227.1','41.0.2228.0','41.0.2272.76',
+#    '42.0.2311.1', '43.0.2357.134', '44.0.2403.130', '45.0.2454.85',
+#)
+
+CHROME_BUILD = (
+    (32, 1700, 1749),
+    (33, 1750, 1846),
+    (34, 1847, 1915),
+    (35, 1916, 1984),
+    (36, 1985, 2061),
+    (37, 2062, 2124),
+    (38, 2125, 2170),
+    (39, 2171, 2213),
+    (40, 2214, 2271),
+    (41, 2272, 2310),
+    (42, 2311, 2357),
 )
 
 
-def generate_navigator(platform=None):
+class UserAgentRuntimeError(Exception):
+    pass
+
+
+def build_chrome_version():
+    build = choice(CHROME_BUILD)
+    return '%d.0.%d.%d' % (
+        build[0],
+        randint(build[1], build[2]),
+        randint(0, 99),
+    )
+
+
+def generate_navigator(platform=None, navigator=None):
     """
     Generates navigator's config
     """
+
+    if navigator != 'firefox':
+        EXCLUDE_PLATFORM = ('mac',)
+    else:
+        EXCLUDE_PLATFORM = ()
+
+    # Process platform option
     if isinstance(platform, six.string_types):
         platform_name = platform
     elif isinstance(platform, (list, tuple)):
-        platform_name = choice(platform)
+        items = [x for x in platform if x not in EXCLUDE_PLATFORM]
+        platform_name = choice(items)
     elif platform is None:
-        platform_name = choice(PLATFORM)
+        items = [x for x in PLATFORM if x not in EXCLUDE_PLATFORM]
+        platform_name = choice(items)
     else:
-        raise Exception('Option platform has invalid value: %s' % platform)
+        raise UserAgentRuntimeError('Option platform has invalid'
+                                    ' value: %s' % platform)
+
+    # Process navigator option
+    if isinstance(navigator, six.string_types):
+        navigator_name = navigator
+    elif isinstance(navigator, (list, tuple)):
+        navigator_name = choice(navigator)
+    elif navigator is None:
+        if platform_name == 'mac':
+            navigator_name = 'firefox'
+        else:
+            navigator_name = choice(NAVIGATOR)
+    else:
+        raise UserAgentRuntimeError('Option navigator has invalid'
+                                    ' value: %s' % navigator)
 
     if platform_name == 'win':
         subplatform, navigator_platform = choice(WINDOWS_SUBPLATFORM)
@@ -98,15 +150,16 @@ def generate_navigator(platform=None):
         navigator_platform = MAC_SUBPLATFORM
         platform = choice(MAC_PLATFORM)
         oscpu = platform[11:]
-    if platform_name == 'mac':
-        navigator_name = 'firefox'
-    else:
-        navigator_name = choice(NAVIGATOR)
-    navigator_version = (
-        choice(FIREFOX_VERSION)
-        if navigator_name == 'firefox' else
-        choice(CHROME_VERSION)
-    )
+
+    if platform_name == 'mac' and navigator_name != 'firefox':
+        raise UserAgentRuntimeError('Only firefox navigator is'
+                                    ' available for mac platform')
+
+    if navigator_name == 'firefox':
+        navigator_version = choice(FIREFOX_VERSION)
+    elif navigator_name == 'chrome':
+        navigator_version = build_chrome_version()
+
     if navigator_name == 'firefox':
         user_agent = FIREFOX_USERAGENT_TEMPLATE % {
             'platform': platform,
@@ -118,6 +171,7 @@ def generate_navigator(platform=None):
             'platform': platform,
             'version': navigator_version,
         }
+
     return {
         'name': navigator_name,
         'version': navigator_version,
