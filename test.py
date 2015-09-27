4da83c3ae5fe4a65259dffe19c234d8a3e5ab6ad
Gregory Petukhov <lorien@lorien.name>
2015-09-28 05:01:25 +0600

    Refactoring

--- test.py
+++ test.py
@@ -33,6 +33,28 @@ class UserAgentTestCase(TestCase):
             ua = generate_user_agent(navigator='chrome')
             self.assertTrue('chrome' in ua.lower())
 
+    def test_navigator_option_tuple(self):
+        for x in range(100):
+            ua = generate_user_agent(navigator=('chrome', 'firefox'))
+
+    def test_platform_option_tuple(self):
+        for x in range(100):
+            ua = generate_user_agent(platform=('win', 'linux'))
+            ua = generate_user_agent(platform=('win', 'linux', 'mac'))
+            ua = generate_user_agent(platform=('win',))
+            ua = generate_user_agent(platform=('linux',))
+            ua = generate_user_agent(platform=('mac',))
+
+    def test_platform_navigator_option(self):
+        for x in range(100):
+            ua = generate_user_agent(platform='win', navigator='firefox')
+            self.assertTrue('firefox' in ua.lower())
+            self.assertTrue('windows' in ua.lower())
+
+            ua = generate_user_agent(platform='win', navigator='chrome')
+            self.assertTrue('chrome' in ua.lower())
+            self.assertTrue('windows' in ua.lower())
+
 
 if __name__ == '__main__':
     unittest.main()
--- user_agent/base.py
+++ user_agent/base.py
@@ -13,63 +13,75 @@ import six
 __all__ = ['generate_user_agent', 'generate_navigator',
            'UserAgentRuntimeError']
 
-PLATFORM = ('linux', 'mac', 'win')
+#PLATFORM = ('linux', 'mac', 'win')
+PLATFORM = {
+    'win': (
+        'Windows NT 5.1', # Windows XP
+        'Windows NT 6.1', # Windows 7
+        'Windows NT 6.2', # Windows 8
+        'Windows NT 6.3', # Windows 8.1
+        'Windows NT 10.0', # Windows 10
+    ),
+    'mac': (
+        'Macintosh; Intel Mac OS X 10.8',
+        'Macintosh; Intel Mac OS X 10.9',
+        'Macintosh; Intel Mac OS X 10.10',
+        'Macintosh; Intel Mac OS X 10.11',
+    ),
+    'linux': (
+        'X11; Linux',
+        'X11; Ubuntu; Linux',
+    ),
+}
+
+SUBPLATFORM = {
+    'win': (
+        ('', 'Win32'), # 32bit
+        ('Win64; x64', 'Win32'), # 64bit
+        ('WOW64', 'Win32'), # 32bit process / 64bit system
+    ),
+    'linux': (
+        ('i686', 'Linux i686'), # 32bit
+        ('x86_64', 'Linux x86_64'), # 64bit
+        ('i686 on x86_64', 'Linux i686 on x86_64'), # 32bit process / 64bit system
+    ),
+    'mac': (
+        ('MacIntel',), # 32bit, 64bit
+    ),
+}
+
+PLATFORM_NAVIGATORS = {
+    'win': ('chrome', 'firefox'),
+    'mac': ('firefox'),
+    'linux': ('chrome', 'firefox'),
+}
+
+NAVIGATOR_PLATFORMS = {
+    'chrome': ('win', 'linux'),
+    'firefox': ('win', 'linux', 'mac'),
+}
+
 NAVIGATOR = ('firefox', 'chrome')
 APPVERSION = '5.0'
-FIREFOX_USERAGENT_TEMPLATE = (
-    'Mozilla/5.0 (%(platform)s; rv:%(version)s) '
-    'Gecko/%(geckotrail)s Firefox/%(version)s'
-)
-CHROME_USERAGENT_TEMPLATE = (
-    'Mozilla/5.0 (%(platform)s) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/%(version)s Safari/537.36'
-)
-WINDOWS_PLATFORM = (
-    'Windows NT 5.1', # Windows XP
-    'Windows NT 6.1', # Windows 7
-    'Windows NT 6.2', # Windows 8
-    'Windows NT 6.3', # Windows 8.1
-    'Windows NT 10.0', # Windows 10
-)
-WINDOWS_SUBPLATFORM = (
-    ('', 'Win32'), # 32bit
-    ('Win64; x64', 'Win32'), # 64bit
-    ('WOW64', 'Win32'), # 32bit process / 64bit system
-)
-MAC_PLATFORM = (
-    'Macintosh; Intel Mac OS X 10.8',
-    'Macintosh; Intel Mac OS X 10.9',
-    'Macintosh; Intel Mac OS X 10.10',
-    'Macintosh; Intel Mac OS X 10.11',
-)
-MAC_SUBPLATFORM = 'MacIntel' # 32bit, 64bit
-LINUX_PLATFORM = (
-    'X11; Linux',
-    'X11; Ubuntu; Linux',
-)
-LINUX_SUBPLATFORM = (
-    ('i686', 'Linux i686'), # 32bit
-    ('x86_64', 'Linux x86_64'), # 64bit
-    ('i686 on x86_64', 'Linux i686 on x86_64'), # 32bit process / 64bit system
-)
+USERAGENT_TEMPLATE = {
+    'firefox': ( 
+        'Mozilla/5.0 (%(platform)s; rv:%(version)s)'
+        ' Gecko/%(geckotrail)s Firefox/%(version)s'
+    ),
+    'chrome': (
+        'Mozilla/5.0 (%(platform)s) AppleWebKit/537.36'
+        ' (KHTML, like Gecko) Chrome/%(version)s Safari/537.36'
+    ),
+}
+
+# https://en.wikipedia.org/wiki/Firefox_release_history
 FIREFOX_VERSION = (
-#    '16.0','18.0','19.0','21.0','22.0','23.0','24.0','25.0',
-    '27.0','28.0','29.0','31.0','33.0','36.0','37.0', '38.0',
-    '39.0','40.0','41.0','42.0','43.0',
+    '27.0', '28.0', '29.0', '31.0', '33.0', '36.0', '37.0', '38.0',
+    '39.0', '40.0', '41.0', '42.0', '43.0',
 )
 GECKOTRAIL_DESKTOP = '20100101'
-#CHROME_VERSION = (
-##    '27.0.1453.116','27.0.1453.90','27.0.1453.93','27.0.1500.55',
-##    '28.0.1464.0','28.0.1467.0','28.0.1468.0','29.0.1547.2','29.0.1547.57',
-##    '29.0.1547.62','30.0.1599.17','31.0.1623.0','31.0.1650.16','32.0.1664.3',
-##    '32.0.1667.0','33.0.1750.517','34.0.1847.116','34.0.1847.137','34.0.1866.237',
-#    '35.0.1916.47','35.0.2117.157','35.0.2309.372','35.0.3319.102','36.0.1944.0',
-#    '36.0.1985.125','36.0.1985.67','37.0.2049.0','37.0.2062.124','41.0.2224.3',
-#    '41.0.2225.0','41.0.2226.0','41.0.2227.1','41.0.2228.0','41.0.2272.76',
-#    '42.0.2311.1', '43.0.2357.134', '44.0.2403.130', '45.0.2454.85',
-#)
 
-#https://en.wikipedia.org/wiki/Google_Chrome_release_history
+# https://en.wikipedia.org/wiki/Google_Chrome_release_history
 CHROME_BUILD = (
     (32, 1700, 1749),
     (33, 1750, 1846),
@@ -81,12 +93,19 @@ CHROME_BUILD = (
     (39, 2171, 2213),
     (40, 2214, 2271),
     (41, 2272, 2310),
-    (42, 2311, 2357),
+    (42, 2311, 2356),
+    (43, 2357, 2402),
+    (44, 2403, 2453),
+    (45, 2454, 2489),
 )
 
 
 class UserAgentRuntimeError(Exception):
     pass
+
+
+def build_firefox_version():
+    return choice(FIREFOX_VERSION)
 
 
 def build_chrome_version():
@@ -101,56 +120,65 @@ def build_chrome_version():
 def generate_navigator(platform=None, navigator=None):
     """
     Generates navigator's config
-    """
 
-    if (navigator and isinstance(navigator, six.string_types)
-            and navigator != 'firefox'):
-        EXCLUDE_PLATFORM = ('mac',)
-    else:
-        EXCLUDE_PLATFORM = ()
+    :param platfrom: limit list of platforms for generation
+    :type platform: None or string or list/tuple
+    :param navigator: limit list of browser engines for generation
+    :type navigator: None or string or list/tuple
+    """
 
     # Process platform option
     if isinstance(platform, six.string_types):
-        platform_name = platform
+        platform_choices = [platform]
     elif isinstance(platform, (list, tuple)):
-        items = [x for x in platform if x not in EXCLUDE_PLATFORM]
-        platform_name = choice(items)
+        platform_choices = list(platform)
     elif platform is None:
-        items = [x for x in PLATFORM if x not in EXCLUDE_PLATFORM]
-        platform_name = choice(items)
+        platform_choices = list(PLATFORM.keys())
     else:
         raise UserAgentRuntimeError('Option platform has invalid'
                                     ' value: %s' % platform)
 
     # Process navigator option
     if isinstance(navigator, six.string_types):
-        navigator_name = navigator
+        navigator_choices = [navigator]
     elif isinstance(navigator, (list, tuple)):
-        navigator_name = choice(navigator)
+        navigator_choices = list(navigator)
     elif navigator is None:
-        if platform_name == 'mac':
-            navigator_name = 'firefox'
-        else:
-            navigator_name = choice(NAVIGATOR)
+        navigator_choices = list(NAVIGATOR)
     else:
         raise UserAgentRuntimeError('Option navigator has invalid'
                                     ' value: %s' % navigator)
 
+    # If we have only one navigator option to choose from
+    # Then use it and select platform from platforms
+    # available for choosen navigator
+    if len(navigator_choices) == 1:
+        navigator_name = navigator_choices[0]
+        platform_name = choice([x for x in platform_choices
+                                if x in NAVIGATOR_PLATFORMS[navigator_name]])
+    else:
+        platform_name = choice(platform_choices)
+        navigator_name = choice([x for x in navigator_choices
+                                 if x in PLATFORM_NAVIGATORS[platform_name]])
+
+    assert platform_name in PLATFORM
+    assert navigator_name in NAVIGATOR
+
     if platform_name == 'win':
-        subplatform, navigator_platform = choice(WINDOWS_SUBPLATFORM)
-        win_platform = choice(WINDOWS_PLATFORM)
+        subplatform, navigator_platform = choice(SUBPLATFORM['win'])
+        win_platform = choice(PLATFORM['win'])
         if subplatform:
             platform = win_platform + '; ' + subplatform
         else:
             platform = win_platform
         oscpu = platform
     elif platform_name == 'linux':
-        subplatform, navigator_platform = choice(LINUX_SUBPLATFORM)
-        platform = choice(LINUX_PLATFORM) + ' ' + subplatform
+        subplatform, navigator_platform = choice(SUBPLATFORM['linux'])
+        platform = choice(PLATFORM['linux']) + ' ' + subplatform
         oscpu = navigator_platform
     elif platform_name == 'mac':
-        navigator_platform = MAC_SUBPLATFORM
-        platform = choice(MAC_PLATFORM)
+        navigator_platform = SUBPLATFORM['mac']
+        platform = choice(PLATFORM['mac'])
         oscpu = platform[11:]
 
     if platform_name == 'mac' and navigator_name != 'firefox':
@@ -158,21 +186,15 @@ def generate_navigator(platform=None, navigator=None):
                                     ' available for mac platform')
 
     if navigator_name == 'firefox':
-        navigator_version = choice(FIREFOX_VERSION)
+        navigator_version = build_firefox_version()
     elif navigator_name == 'chrome':
         navigator_version = build_chrome_version()
 
-    if navigator_name == 'firefox':
-        user_agent = FIREFOX_USERAGENT_TEMPLATE % {
-            'platform': platform,
-            'version': navigator_version,
-            'geckotrail': GECKOTRAIL_DESKTOP,
-        }
-    elif navigator_name == 'chrome':
-        user_agent = CHROME_USERAGENT_TEMPLATE % {
-            'platform': platform,
-            'version': navigator_version,
-        }
+    user_agent = USERAGENT_TEMPLATE[navigator_name] % {
+        'platform': platform,
+        'version': navigator_version,
+        'geckotrail': GECKOTRAIL_DESKTOP,
+    }
 
     return {
         'name': navigator_name,
