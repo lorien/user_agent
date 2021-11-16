11c30f5dd765475a9f5f0f847f31c47af8c40a39
immerrr again <immerrr@gmail.com>
2021-11-16 13:37:36 +0100

    Fix uses of file objects

--- user_agent/device.py
+++ user_agent/device.py
@@ -4,6 +4,6 @@ import json
 
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(PACKAGE_DIR, 'data/smartphone_dev_id.json')) as f:
-    SMARTPHONE_DEV_IDS = json.load(open(f))
+    SMARTPHONE_DEV_IDS = json.load(f)
 with open(os.path.join(PACKAGE_DIR, 'data/tablet_dev_id.json')) as f:
-    TABLET_DEV_IDS = json.load(open())
+    TABLET_DEV_IDS = json.load(f)
