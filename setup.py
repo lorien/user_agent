98c22fa73098a666e399083e03d3502f1246fc82
User <guest@example.com>
2025-09-08 23:32:49 +0300

    Fix setup.py to install data files

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.12",
     packages=["user_agent", "user_agent.data"],
     install_requires=["six", "pytz"],
+    include_package_data=True,
     entry_points={
         "console_scripts": {
             "ua=user_agent.cli:script_ua",
